0de511b529fca18fdf6269cf47928aea16403a3b
Alexander Higgins <alexhiggins732@hotmail.com>
2021-01-12 03:15:49 -0500

    Add project files.

---
+++ OfflineCrypto/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace RsaCrypto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrivate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nmKeySize = new System.Windows.Forms.NumericUpDown();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnAesDecrypt = new System.Windows.Forms.Button();
+            this.txtAesBase64 = new System.Windows.Forms.TextBox();
+            this.btnAesEncrypt = new System.Windows.Forms.Button();
+            this.txtAesPlain = new System.Windows.Forms.TextBox();
+            this.lblAesDecoded = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmKeySize)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(6, 19);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(660, 20);
+            this.txtFile.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(672, 16);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnDecrypt);
+            this.groupBox1.Controls.Add(this.btnEncrypt);
+            this.groupBox1.Controls.Add(this.txtFile);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Location = new System.Drawing.Point(12, 371);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(756, 80);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDecrypt.Location = new System.Drawing.Point(478, 45);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncrypt.Location = new System.Drawing.Point(559, 45);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtPrivate);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.nmKeySize);
+            this.groupBox2.Controls.Add(this.btnGenerate);
+            this.groupBox2.Controls.Add(this.txtKey);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(753, 246);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Generate Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Private:";
+            // 
+            // txtPrivate
+            // 
+            this.txtPrivate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrivate.Location = new System.Drawing.Point(6, 130);
+            this.txtPrivate.Multiline = true;
+            this.txtPrivate.Name = "txtPrivate";
+            this.txtPrivate.Size = new System.Drawing.Size(741, 84);
+            this.txtPrivate.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Public:";
+            // 
+            // nmKeySize
+            // 
+            this.nmKeySize.Increment = new decimal(new int[] {
+            2048,
+            0,
+            0,
+            0});
+            this.nmKeySize.Location = new System.Drawing.Point(6, 220);
+            this.nmKeySize.Maximum = new decimal(new int[] {
+            16384,
+            0,
+            0,
+            0});
+            this.nmKeySize.Minimum = new decimal(new int[] {
+            16384,
+            0,
+            0,
+            0});
+            this.nmKeySize.Name = "nmKeySize";
+            this.nmKeySize.Size = new System.Drawing.Size(120, 20);
+            this.nmKeySize.TabIndex = 3;
+            this.nmKeySize.Value = new decimal(new int[] {
+            16384,
+            0,
+            0,
+            0});
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerate.Location = new System.Drawing.Point(672, 217);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKey.Location = new System.Drawing.Point(6, 36);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtKey.Size = new System.Drawing.Size(741, 74);
+            this.txtKey.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lblAesDecoded);
+            this.groupBox3.Controls.Add(this.btnAesDecrypt);
+            this.groupBox3.Controls.Add(this.txtAesBase64);
+            this.groupBox3.Controls.Add(this.btnAesEncrypt);
+            this.groupBox3.Controls.Add(this.txtAesPlain);
+            this.groupBox3.Location = new System.Drawing.Point(12, 264);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(756, 101);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "AES Key";
+            // 
+            // btnAesDecrypt
+            // 
+            this.btnAesDecrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAesDecrypt.Location = new System.Drawing.Point(591, 71);
+            this.btnAesDecrypt.Name = "btnAesDecrypt";
+            this.btnAesDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnAesDecrypt.TabIndex = 7;
+            this.btnAesDecrypt.Text = "Decrypt";
+            this.btnAesDecrypt.UseVisualStyleBackColor = true;
+            this.btnAesDecrypt.Click += new System.EventHandler(this.btnAesDecrypt_Click);
+            // 
+            // txtAesBase64
+            // 
+            this.txtAesBase64.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAesBase64.Location = new System.Drawing.Point(77, 45);
+            this.txtAesBase64.Name = "txtAesBase64";
+            this.txtAesBase64.Size = new System.Drawing.Size(670, 20);
+            this.txtAesBase64.TabIndex = 6;
+            // 
+            // btnAesEncrypt
+            // 
+            this.btnAesEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAesEncrypt.Location = new System.Drawing.Point(675, 71);
+            this.btnAesEncrypt.Name = "btnAesEncrypt";
+            this.btnAesEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnAesEncrypt.TabIndex = 5;
+            this.btnAesEncrypt.Text = "Encrypt";
+            this.btnAesEncrypt.UseVisualStyleBackColor = true;
+            this.btnAesEncrypt.Click += new System.EventHandler(this.btnAesEncrypt_Click);
+            // 
+            // txtAesPlain
+            // 
+            this.txtAesPlain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAesPlain.Location = new System.Drawing.Point(77, 19);
+            this.txtAesPlain.Name = "txtAesPlain";
+            this.txtAesPlain.Size = new System.Drawing.Size(670, 20);
+            this.txtAesPlain.TabIndex = 0;
+            // 
+            // lblAesDecoded
+            // 
+            this.lblAesDecoded.AutoSize = true;
+            this.lblAesDecoded.Location = new System.Drawing.Point(6, 24);
+            this.lblAesDecoded.Name = "lblAesDecoded";
+            this.lblAesDecoded.Size = new System.Drawing.Size(46, 13);
+            this.lblAesDecoded.TabIndex = 8;
+            this.lblAesDecoded.Text = "Aes Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Aes Base64";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 467);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nmKeySize)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.NumericUpDown nmKeySize;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrivate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtAesBase64;
+        private System.Windows.Forms.Button btnAesEncrypt;
+        private System.Windows.Forms.TextBox txtAesPlain;
+        private System.Windows.Forms.Button btnAesDecrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAesDecoded;
+    }
+}
+
---
+++ OfflineCrypto/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RsaCrypto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int KeySize => (int)nmKeySize.Value;
+        public string GetXml(RSA rsa, bool includePrivate)
+        {
+            RSAParameters rsaKeyInfo = rsa.ExportParameters(includePrivate);
+            string publicPrivateKeyXML = rsa.ToXmlString(includePrivate);
+            var doc = new System.Xml.XmlDocument();
+            doc.LoadXml(publicPrivateKeyXML);
+
+            using (var ms = new MemoryStream())
+            using (var writer = new XmlTextWriter(ms, Encoding.Unicode))
+            {
+                writer.Formatting = Formatting.Indented;
+
+                // Write the XML into a formatting XmlTextWriter
+                doc.WriteContentTo(writer);
+                writer.Flush();
+                ms.Flush();
+                ms.Position = 0;
+
+                // Read MemoryStream contents into a StreamReader.
+                StreamReader sReader = new StreamReader(ms);
+
+                // Extract the text from the StreamReader.
+                string formattedXml = sReader.ReadToEnd();
+
+                return formattedXml;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Generate a public/private key pair.  
+            RSA rsa = RSA.Create();
+            rsa.KeySize = KeySize;
+            //Save the public key information to an RSAParameters structure.  
+
+            txtKey.Text = GetXml(rsa, false);
+            txtPrivate.Text = GetXml(rsa, true);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFile.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnAesEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtAesPlain.Text))
+            {
+
+                MessageBox.Show("Enter an AES Password of at least 32 characters");
+                return;
+            }
+            using (var rsa = new RSACryptoServiceProvider(KeySize))
+            {
+                rsa.FromXmlString(txtKey.Text);
+                var data = System.Text.Encoding.UTF8.GetBytes(txtAesPlain.Text);
+                var encryptedData = rsa.Encrypt(data, true);
+                var base64Encrypted = Convert.ToBase64String(encryptedData);
+                txtAesBase64.Text = base64Encrypted;
+            }
+        }
+
+        private void btnAesDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPrivate.Text))
+            {
+                MessageBox.Show("Please enter private key RSA xml", "Error");
+                return;
+            }
+            try
+            {
+                var doc = new XmlDocument();
+                doc.LoadXml(txtPrivate.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error parsing private key xml: {ex.Message}", "Error");
+            }
+            try
+            {
+                var bytes = Convert.FromBase64String(txtAesBase64.Text.Trim());
+                using (var rsa = new RSACryptoServiceProvider(KeySize))
+                {
+                    rsa.FromXmlString(txtPrivate.Text);
+                    var decrypted = rsa.Decrypt(bytes, true);
+                    var plain = System.Text.Encoding.UTF8.GetString(decrypted);
+                    txtAesPlain.Text = plain;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+
+        }
+
+        byte[] Key = new byte[] { };
+        byte[] IV = new byte[] { };
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtFile.Text))
+            {
+                MessageBox.Show("Please select a valid file");
+                return;
+            }
+
+
+            AES_Encrypt(txtFile.Text, txtAesPlain.Text);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtFile.Text))
+            {
+                MessageBox.Show("Please select a valid file");
+                return;
+            }
+
+
+            AES_Decrypt(txtFile.Text, txtAesPlain.Text);
+        }
+
+
+        private void AES_Encrypt(string inputFile, string password)
+        {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+
+            var fi = new FileInfo(inputFile);
+            var dir = fi.Directory;
+            var filename = Path.GetFileNameWithoutExtension(fi.Name);
+            var dest = Path.Combine(dir.FullName, $"{filename}-{DateTime.Now.ToFileTime()}{fi.Extension}.aes");
+            //create output file name
+            FileStream fsCrypt = new FileStream(dest, FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CFB;
+
+            //write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                    cs.Write(buffer, 0, read);
+                }
+
+                //close up
+                fsIn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                return;
+                //Debug.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+            MessageBox.Show($"Encrypted file {dest}", "Done");
+
+        }
+
+        private void AES_Decrypt(string inputFile, string password)
+        {
+            //todo:
+            // - create error message on wrong password
+            // - on cancel: close and delete file
+            // - on wrong password: close and delete file!
+            // - create a better filen name
+            // - could be check md5 hash on the files but it make this slow
+
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            var fi = new FileInfo(inputFile);
+            var dir = fi.Directory;
+            var filename = Path.GetFileNameWithoutExtension(fi.Name);
+            var fileNameRoot = Path.GetFileNameWithoutExtension(filename);
+            var dest = Path.Combine(dir.FullName, $"{filename}");
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(dest, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (System.Security.Cryptography.CryptographicException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                return;
+                //Debug.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                //Debug.WriteLine("Error: " + ex.Message);
+                MessageBox.Show(ex.Message, "Error");
+                return;
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                return;
+                //Debug.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+            MessageBox.Show($"Decrypted file {dest}", "Done");
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            //Source: http://www.dotnetperls.com/rngcryptoserviceprovider
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                // Ten iterations.
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fill buffer.
+                    rng.GetBytes(data);
+                }
+            }
+            return data;
+        }
+
+
+    }
+
+}
